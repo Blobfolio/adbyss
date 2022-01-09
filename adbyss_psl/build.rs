79ef407745df0855c3205c89069e27143b13fd36
Josh Stoik <josh@blobfolio.com>
2022-01-08 21:12:40 -0800

    cleanup: alphabetize methods

--- adbyss_psl/build.rs
+++ adbyss_psl/build.rs
@@ -68,84 +68,6 @@ pub fn main() {
 		.expect("Unable to save reference list.");
 }
 
-#[cfg(not(feature = "docs-workaround"))]
-/// # Load Data.
-fn load_data() -> (RawMainMap, RawWildMap) {
-	// Let's build the thing we'll be writing about building.
-	let mut psl_main: RawMainMap = HashSet::new();
-	let mut psl_wild: RawWildMap = HashMap::new();
-
-	const FLAG_EXCEPTION: u8 = 0b0001;
-	const FLAG_WILDCARD: u8  = 0b0010;
-
-	// Parse the raw data.
-	fetch_suffixes().lines()
-		.filter(|line| ! line.is_empty() && ! line.starts_with("//"))
-		.filter_map(|mut line| {
-			let mut flags: u8 = 0;
-
-			if line.starts_with('!') {
-				line = &line[1..];
-				flags |= FLAG_EXCEPTION;
-			}
-
-			if line.starts_with("*.") {
-				line = &line[2..];
-				flags |= FLAG_WILDCARD;
-			}
-
-			// To correctly handle the suffixes, we'll need to prepend a
-			// hypothetical root and strip it off after.
-			idna::domain_to_ascii_strict(&["one.two.", line].concat())
-				.ok()
-				.map(|mut x| x.split_off(8))
-				.zip(Some(flags))
-		})
-		.for_each(|(host, flag)|
-			// This is a wildcard exception.
-			if 0 != flag & FLAG_EXCEPTION {
-				if let Some(idx) = host.as_bytes()
-					.iter()
-					.position(|x| x == &b'.')
-				{
-					let (before, after) = host.split_at(idx);
-					psl_wild.entry(after[1..].to_string())
-						.or_insert_with(Vec::new)
-						.push(before.to_string());
-				}
-			}
-			// This is the main wildcard entry.
-			else if 0 != flag & FLAG_WILDCARD {
-				psl_wild.entry(host)
-					.or_insert_with(Vec::new);
-			}
-			// This is a normal suffix.
-			else {
-				psl_main.insert(host);
-			}
-		);
-
-	(psl_main, psl_wild)
-}
-
-#[cfg(feature = "docs-workaround")]
-/// # (Fake) Load Data.
-///
-/// This is a network-free workaround to allow `docs.rs` to be able to generate
-/// documentation for this library.
-///
-/// Don't try to compile this library with the `docs-workaround` feature or the
-/// library won't work properly.
-fn load_data() -> (RawMainMap, RawWildMap) {
-	let mut psl_main: RawMainMap = HashSet::new();
-	psl_main.insert(String::from("com"));
-
-	let mut psl_wild: RawWildMap = HashMap::new();
-	psl_wild.insert(String::from("bd"), Vec::new());
-
-	(psl_main, psl_wild)
-}
-
 /// # Build List.
 ///
 /// This takes the lightly-processed main and wild lists, and generates all the
@@ -368,6 +290,84 @@ fn format_wild_arm(src: &[String]) -> String {
 		.collect();
 	out.sort();
 	out.join(" | ")
+}
+
+#[cfg(not(feature = "docs-workaround"))]
+/// # Load Data.
+fn load_data() -> (RawMainMap, RawWildMap) {
+	// Let's build the thing we'll be writing about building.
+	let mut psl_main: RawMainMap = HashSet::new();
+	let mut psl_wild: RawWildMap = HashMap::new();
+
+	const FLAG_EXCEPTION: u8 = 0b0001;
+	const FLAG_WILDCARD: u8  = 0b0010;
+
+	// Parse the raw data.
+	fetch_suffixes().lines()
+		.filter(|line| ! line.is_empty() && ! line.starts_with("//"))
+		.filter_map(|mut line| {
+			let mut flags: u8 = 0;
+
+			if line.starts_with('!') {
+				line = &line[1..];
+				flags |= FLAG_EXCEPTION;
+			}
+
+			if line.starts_with("*.") {
+				line = &line[2..];
+				flags |= FLAG_WILDCARD;
+			}
+
+			// To correctly handle the suffixes, we'll need to prepend a
+			// hypothetical root and strip it off after.
+			idna::domain_to_ascii_strict(&["one.two.", line].concat())
+				.ok()
+				.map(|mut x| x.split_off(8))
+				.zip(Some(flags))
+		})
+		.for_each(|(host, flag)|
+			// This is a wildcard exception.
+			if 0 != flag & FLAG_EXCEPTION {
+				if let Some(idx) = host.as_bytes()
+					.iter()
+					.position(|x| x == &b'.')
+				{
+					let (before, after) = host.split_at(idx);
+					psl_wild.entry(after[1..].to_string())
+						.or_insert_with(Vec::new)
+						.push(before.to_string());
+				}
+			}
+			// This is the main wildcard entry.
+			else if 0 != flag & FLAG_WILDCARD {
+				psl_wild.entry(host)
+					.or_insert_with(Vec::new);
+			}
+			// This is a normal suffix.
+			else {
+				psl_main.insert(host);
+			}
+		);
+
+	(psl_main, psl_wild)
+}
+
+#[cfg(feature = "docs-workaround")]
+/// # (Fake) Load Data.
+///
+/// This is a network-free workaround to allow `docs.rs` to be able to generate
+/// documentation for this library.
+///
+/// Don't try to compile this library with the `docs-workaround` feature or the
+/// library won't work properly.
+fn load_data() -> (RawMainMap, RawWildMap) {
+	let mut psl_main: RawMainMap = HashSet::new();
+	psl_main.insert(String::from("com"));
+
+	let mut psl_wild: RawWildMap = HashMap::new();
+	psl_wild.insert(String::from("bd"), Vec::new());
+
+	(psl_main, psl_wild)
 }
 
 /// # Out path.
