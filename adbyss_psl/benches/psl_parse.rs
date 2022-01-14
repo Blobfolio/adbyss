38859824b50639827bfa6c35be023c086ca327c9
Josh Stoik <josh@blobfolio.com>
2022-01-14 09:20:36 -0800

    misc: bench puny

--- adbyss_psl/benches/psl_parse.rs
+++ adbyss_psl/benches/psl_parse.rs
@@ -21,4 +21,8 @@ benches!(
 	Bench::new("adbyss_psl::Domain", "new(食狮.com.cn)")
 		.timed(Duration::from_secs(2))
 		.with(|| Domain::new("食狮.com.cn")),
+
+	Bench::new("adbyss_psl::Domain", "new(xn--85x722f.com.cn)")
+		.timed(Duration::from_secs(2))
+		.with(|| Domain::new("xn--85x722f.com.cn")),
 );
