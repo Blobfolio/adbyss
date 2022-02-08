8cd146bd20621d0d74e46dd97e9a1d73d1ae0464
Josh Stoik <josh@blobfolio.com>
2022-02-08 13:25:08 -0800

    misc: try to fix github action weirdness

--- adbyss_core/src/sources.rs
+++ adbyss_core/src/sources.rs
@@ -15,6 +15,7 @@ use crate::{
 use rayon::{
 	iter::{
 		IntoParallelIterator,
+		IntoParallelRefIterator,
 		ParallelExtend,
 		ParallelIterator,
 	},
@@ -177,12 +178,11 @@ impl Source {
 	///
 	/// This returns an error if any source data could be downloaded or parsed.
 	pub fn fetch_many(src: u8) -> Result<HashSet<Domain, ahash::RandomState>, AdbyssError> {
-		let all: [Self; 5] = [Self::AdAway, Self::Adbyss, Self::StevenBlack, Self::YouTube, Self::Yoyo];
-
 		// First, build a consolidated string of all the entries.
-		let raw: Cow<str> = all.into_par_iter()
-			.filter(|x: &Self| 0 != src & (*x as u8))
-			.map(|x: Self| x.fetch_raw())
+		let raw: Cow<str> = [Self::AdAway, Self::Adbyss, Self::StevenBlack, Self::YouTube, Self::Yoyo].par_iter()
+			.copied()
+			.filter(|x| 0 != src & (*x as u8))
+			.map(Self::fetch_raw)
 			// Merge the raw data into a single block so we can better
 			// parallelize parsing. If any sources failed, operations will
 			// abort here.
