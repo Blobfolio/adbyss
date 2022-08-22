8bbc6664a55ba79d7bd91a21c31b29fce771da2f
Josh Stoik <josh@blobfolio.com>
2022-08-22 13:54:42 -0700

    bump: brunch 0.3

--- adbyss_psl/benches/psl_parse.rs
+++ adbyss_psl/benches/psl_parse.rs
@@ -7,22 +7,17 @@ use brunch::{
 	benches,
 };
 use adbyss_psl::Domain;
-use std::time::Duration;
 
 benches!(
-	Bench::new("adbyss_psl::Domain", "new(blobfolio.com)")
-		.timed(Duration::from_secs(1))
-		.with(|| Domain::new("blobfolio.com")),
+	Bench::new("adbyss_psl::Domain::new(blobfolio.com)")
+		.run(|| Domain::new("blobfolio.com")),
 
-	Bench::new("adbyss_psl::Domain", "new(www.blobfolio.com)")
-		.timed(Duration::from_secs(1))
-		.with(|| Domain::new("www.blobfolio.com")),
+	Bench::new("adbyss_psl::Domain::new(www.blobfolio.com)")
+		.run(|| Domain::new("www.blobfolio.com")),
 
-	Bench::new("adbyss_psl::Domain", "new(食狮.com.cn)")
-		.timed(Duration::from_secs(1))
-		.with(|| Domain::new("食狮.com.cn")),
+	Bench::new("adbyss_psl::Domain::new(食狮.com.cn)")
+		.run(|| Domain::new("食狮.com.cn")),
 
-	Bench::new("adbyss_psl::Domain", "new(xn--85x722f.com.cn)")
-		.timed(Duration::from_secs(1))
-		.with(|| Domain::new("xn--85x722f.com.cn")),
+	Bench::new("adbyss_psl::Domain::new(xn--85x722f.com.cn)")
+		.run(|| Domain::new("xn--85x722f.com.cn")),
 );
