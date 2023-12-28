f9d67c596968ac98856e3c0b1bd680fab1da4614
Josh Stoik <josh@blobfolio.com>
2023-12-28 10:50:43 -0800

    lint

--- adbyss_psl/src/puny.rs
+++ adbyss_psl/src/puny.rs
@@ -206,7 +206,7 @@ fn decode_digit(c: char) -> u32 {
 }
 
 #[inline]
-fn value_to_digit(value: u32) -> char {
+const fn value_to_digit(value: u32) -> char {
 	match value {
 		0..=25 => (value as u8 + b'a') as char,
 		26..=35 => (value as u8 - 26 + b'0') as char,
