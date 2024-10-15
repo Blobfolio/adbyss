5d8b8bfc64a7dd65182ee1effc21233f14e45d34
Josh Stoik <josh@blobfolio.com>
2024-10-14 22:22:50 -0700

    bump: argyle 0.10 refactor: cli parsing

---
+++ adbyss/build.rs
@@ -0,0 +1,34 @@
+/*!
+# Adbyss: Build
+*/
+
+use argyle::KeyWordsBuilder;
+use std::path::PathBuf;
+
+
+
+/// # Set Up CLI Arguments.
+pub fn main() {
+	let mut builder = KeyWordsBuilder::default();
+	builder.push_keys([
+		"--disable",
+		"-h", "--help",
+		"-q", "--quiet",
+		"--show",
+		"--stdout",
+		"--systemd",
+		"-V", "--version",
+		"-y", "--yes",
+	]);
+	builder.push_keys_with_values(["-c", "--config"]);
+	builder.save(out_path("argyle.rs"));
+}
+
+/// # Output Path.
+///
+/// Append the sub-path to OUT_DIR and return it.
+fn out_path(stub: &str) -> PathBuf {
+	std::fs::canonicalize(std::env::var("OUT_DIR").expect("Missing OUT_DIR."))
+		.expect("Missing OUT_DIR.")
+		.join(stub)
+}
