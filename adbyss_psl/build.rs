f6587168163844452d779c8db9470b582b13994b
Josh Stoik <josh@blobfolio.com>
2024-10-21 00:04:48 -0700

    cleanup: handle codegen in one go misc: make exception matching const

--- adbyss_psl/build.rs
+++ adbyss_psl/build.rs
@@ -6,6 +6,7 @@ use std::{
 	borrow::Cow,
 	collections::{
 		BTreeMap,
+		BTreeSet,
 		HashMap,
 		HashSet,
 	},
@@ -33,7 +34,6 @@ type RawWildMap = HashMap<String, Vec<String>>;
 /// * IDNA/Unicode unit tests;
 pub fn main() {
 	println!("cargo:rerun-if-env-changed=CARGO_PKG_VERSION");
-	println!("cargo:rerun-if-changed=skel/psl.rs.txt");
 	println!("cargo:rerun-if-changed=skel/raw/public_suffix_list.dat");
 
 	psl();
@@ -63,71 +63,59 @@ fn psl() {
 	}
 
 	// Reformat it.
-	let (
-		map,
-		wild_kinds,
-		wild_arms,
-	) = psl_build_list(&psl_main, &psl_wild);
+	let out = psl_build(&psl_main, &psl_wild);
 
 	// Our generated script will live here.
-	let mut file = File::create(out_path("adbyss-psl.rs"))
-		.expect("Unable to create adbyss-psl.rs");
-
-	// Save it!
-	write!(
-		&mut file,
-		include_str!("./skel/psl.rs.txt"),
-		map = map,
-		wild_kinds = wild_kinds,
-		wild_arms = wild_arms,
-	)
-		.and_then(|_| file.flush())
+	File::create(out_path("adbyss-psl.rs"))
+		.and_then(|mut file|
+			file.write_all(out.as_bytes()).and_then(|()| file.flush())
+		)
 		.expect("Unable to save reference list.");
 }
 
-/// # Build List.
+/// # Codegen.
 ///
-/// This method crunches the (pre-filtered) Public Suffix data into a static
-/// hash map we can query at runtime.
+/// This method crunches the (pre-filtered) Public Suffix data into static maps
+/// we can query at runtime, and provides methods for querying them.
 ///
 /// Ultimately, there are three kinds of entries:
 /// * TLD: a normal TLD.
 /// * Wild: a TLD that comprises both the explicit entry, as well as any arbitrary "subdomain".
 /// * Wild-But: a Wild entry that contains one or more exceptions to chunks that may precede it.
-fn psl_build_list(main: &RawMainMap, wild: &RawWildMap) -> (String, String, String) {
+fn psl_build(main: &RawMainMap, wild: &RawWildMap) -> String {
 	use std::fmt::Write;
 
-	// The wild stuff is the hardest.
-	let (wild_map, wild_kinds, wild_arms) = psl_build_wild(wild);
-
-	// We assume com/net/org are normal; let's verify that!
-	for i in ["com", "net", "org"] {
-		assert!(main.contains(i), "Normal tld list missing {i}!");
-		assert!(! wild.contains_key(i), "Wild list contains {i}!");
-	}
+	// Pre-stringify and count the wildcard exceptions. There should only be
+	// a couple of them.
+	let wild_ex: BTreeMap<String, usize> = wild.values()
+		.filter_map(|ex| psl_format_wild(ex))
+		.collect::<BTreeSet<String>>()
+		.into_iter()
+		.enumerate()
+		.map(|(v, k)| (k, v))
+		.collect();
 
 	// Combine the main and wild data into a single, deduped map, sorted for
-	// binary search compatibility, which is how lookups will end up working on
-	// the runtime side of the equation.
+	// binary search compatibility.
+	assert!(main.contains("com"), "Normal tld list missing COM!");
 	let mut map: BTreeMap<&str, Cow<str>> = main.iter()
 		.map(|host| (host.as_str(), Cow::Borrowed("SuffixKind::Tld")))
 		.chain(
-			wild.iter().map(|(host, ex)| {
-				let hash = host.as_str();
-				if ex.is_empty() {
-					(hash, Cow::Borrowed("SuffixKind::Wild"))
-				}
+			wild.iter().map(|(host, ex)|
+				if ex.is_empty() { (host.as_str(), Cow::Borrowed("SuffixKind::Wild")) }
 				else {
-					let ex = psl_format_wild(ex);
-					let ex = wild_map.get(&ex).expect("Missing wild arm.");
-					(hash, Cow::Owned(format!("SuffixKind::WildEx(WildKind::{ex})")))
+					// Allocating a new string for the lookup sucks, but there
+					// aren't many exceptions so this doesn't run often.
+					let ex = psl_format_wild(ex).and_then(|ex| wild_ex.get(&ex))
+						.expect("Missing wild arm.");
+					(host.as_str(), Cow::Owned(format!("SuffixKind::WildEx(WildKind::Ex{ex})")))
 				}
-			})
+			)
 		)
 		.collect();
 
-	// Almost half of all domain registrations use .com; we've got specialized
-	// match logic for it so can leave it out of the map.
+	// Almost half of all domain registrations use .com; we specialize its
+	// matching so can remove it from the lookup map.
 	map.remove("com");
 
 	// Make sure we didn't lose anything.
@@ -145,18 +133,19 @@ fn psl_build_list(main: &RawMainMap, wild: &RawWildMap) -> (String, String, Stri
 		grouped.entry(count).or_default().insert(k, v);
 	}
 
-	// The *real* maximum part limit is one higher than we found above since
-	// wildcards can match anything.
-	let max_parts = grouped.keys().last().unwrap() + 1;
+	// Generate the MAX_PARTS const so we know how much suffix splitting to
+	// do when searching for matches. Note that this is one larger than the
+	// discovered count because wildcards require an extra part.
 	let mut out = String::with_capacity(365_000);
 	writeln!(
 		&mut out,
 		"/// # Max Parts (Matchable).
-const MAX_PARTS: usize = {max_parts};
-"
+const MAX_PARTS: usize = {};
+",
+		grouped.keys().last().unwrap() + 1
 	).unwrap();
 
-	// Generate constant maps for each of the grouped suffixes.
+	// Generate MAP_## const for each of the grouped suffix/kind sets.
 	for (count, set) in grouped.iter() {
 		writeln!(
 			&mut out,
@@ -175,7 +164,29 @@ const MAP_{count}: &[(&[u8], SuffixKind)] = &[",
 		out.push_str("];\n\n");
 	}
 
-	// Generate a finder helper that links a count to the appropriate map.
+	// Generate the WildKind enum.
+	out.push_str(r#"#[derive(Clone, Copy)]
+/// # Wild Kind Exceptions.
+pub(super) enum WildKind {"#);
+	for ex in wild_ex.values() {
+		writeln!(&mut out, "\n\t/// # Exception #{ex}.\n\tEx{ex},").unwrap();
+	}
+	out.push_str("}
+
+impl WildKind {
+	/// # Is Exception?
+	pub(super) const fn is_match(self, src: &[u8]) -> bool {
+		match self {
+");
+	for (cond, ex) in wild_ex.iter() {
+		writeln!(&mut out, "\t\t\tSelf::Ex{ex} => {cond},").unwrap();
+	}
+	out.push_str("\t\t}
+	}
+}");
+
+	// Lastly, generate a method to perform suffix-to-kind searches (using the
+	// previously-generated maps).
 	out.push_str(
 "impl SuffixKind {
 	/// # Suffix Kind From Slice.
@@ -188,7 +199,7 @@ const MAP_{count}: &[(&[u8], SuffixKind)] = &[",
 		if *count == 1 {
 			out.push_str("\t\t\t1 =>
 \t\t\t\tif src == b\"com\" { return Some(Self::Tld); }
-\t\t\t\telse { MAP_1 },");
+\t\t\t\telse { MAP_1 },\n");
 		}
 		// Otherwise just map the map.
 		else {
@@ -202,56 +213,15 @@ const MAP_{count}: &[(&[u8], SuffixKind)] = &[",
 \t}\n}\n\n");
 
 	// Return everything!
-	(out, wild_kinds, wild_arms)
-}
-
-/// # Build Wild Enum.
-///
-/// There aren't very many wildcard exceptions, so we end up storing them as a
-/// static enum at runtime. A matcher function is generated with the
-/// appropriate branch tests, which will either be a straight slice comparison
-/// or a `[].contains`-type match.
-fn psl_build_wild(wild: &RawWildMap) -> (HashMap<String, String>, String, String) {
-	// Let's start with the wild kinds and wild arms.
-	let mut tmp: Vec<String> = wild.values()
-		.filter_map(|ex|
-			if ex.is_empty() { None }
-			else { Some(psl_format_wild(ex)) }
-		)
-		.collect();
-	tmp.sort();
-	tmp.dedup();
-
-	let mut wild_kinds: Vec<String> = Vec::new();
-	let mut wild_map: HashMap<String, String> = HashMap::new();
-	for (k, v) in tmp.into_iter().enumerate() {
-		let name = format!("Ex{k}");
-		wild_kinds.push(format!("{name},"));
-		wild_map.insert(v, name);
-	}
-
-	// If there aren't any wild exceptions, we can just return an empty
-	// placeholder that will never be referenced.
-	if wild_kinds.is_empty() {
-		return (wild_map, "\tNone,".to_owned(), "\t\t\tSelf::None => false,".to_owned());
-	}
-
-	let wild_kinds: String = wild_kinds.join("\n");
-	let mut wild_arms: Vec<(&String, &String)> = wild_map.iter().collect();
-	wild_arms.sort_by(|a, b| a.1.cmp(b.1));
-	let wild_arms = wild_arms.into_iter()
-		.map(|(cond, name)| format!("\t\t\tSelf::{name} => {cond},"))
-		.collect::<Vec<String>>()
-		.join("\n");
-
-	(wild_map, wild_kinds, wild_arms)
+	out
 }
 
 /// # Fetch Suffixes.
 ///
 /// This loads and lightly cleans the raw Public Suffix List data.
 fn psl_fetch_suffixes() -> String {
-	let mut raw = load_file("public_suffix_list.dat");
+	let mut raw = std::fs::read_to_string("skel/raw/public_suffix_list.dat")
+		.expect("Unable to load public_suffix_list.dat");
 
 	// Remove leading whitespace at the start of each line.
 	let mut last = '\n';
@@ -274,25 +244,22 @@ fn psl_fetch_suffixes() -> String {
 
 /// # Format Wild Exceptions.
 ///
-/// This builds the match condition for a wildcard exception, which will either
-/// take the form of a straight slice comparison, or a `[].contains` match.
-///
-/// Not all wildcards have exceptions. Just in case, this method will return an
-/// empty string in such cases, but those will get filtered out during
-/// processing.
-fn psl_format_wild(src: &[String]) -> String {
-	if src.is_empty() { String::new() }
-	else if src.len() == 1 {
-		format!("src == b\"{}\"", src[0])
-	}
+/// This builds the match condition for a wildcard's exceptions, if any.
+/// (There aren't very many.)
+fn psl_format_wild(src: &[String]) -> Option<String> {
+	use std::fmt::Write;
+
+	if src.is_empty() { None }
 	else {
-		format!(
-			"[{}].contains(src)",
-			src.iter()
-				.map(|s| format!("b\"{s}\""))
-				.collect::<Vec<String>>()
-				.join(", ")
-		)
+		let mut out = "matches!(src, ".to_owned();
+		let mut iter = src.iter();
+		let next = iter.next()?;
+		write!(&mut out, "b\"{next}\"").ok()?;
+		for next in iter {
+			write!(&mut out, " | b\"{next}\"").ok()?;
+		}
+		out.push(')');
+		Some(out)
 	}
 }
 
@@ -378,16 +345,6 @@ fn psl_load_data() -> (RawMainMap, RawWildMap) {
 }
 
 
-
-/// # Load File.
-///
-/// Read the third-party data file into a string.
-fn load_file(name: &str) -> String {
-	match std::fs::read_to_string(format!("./skel/raw/{name}")) {
-		Ok(x) => x,
-		Err(_) => panic!("Unable to load {name}."),
-	}
-}
 
 /// # Out path.
 ///
