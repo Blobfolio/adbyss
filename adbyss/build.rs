84cbac9b74789fba55537875239114a971e1ee48
Josh Stoik <josh@blobfolio.com>
2025-03-11 19:31:43 -0700

    bump: argyle 0.12 misc: use argyle::FlagsBuilder for flags

--- adbyss/build.rs
+++ adbyss/build.rs
@@ -2,15 +2,24 @@
 # Adbyss: Build
 */
 
-use argyle::KeyWordsBuilder;
+use argyle::{
+	FlagsBuilder,
+	KeyWordsBuilder,
+};
 use std::path::PathBuf;
 
 
 
-/// # Set Up CLI Arguments.
+/// # Set Up CLI Arguments and Flags.
 pub fn main() {
 	println!("cargo:rerun-if-env-changed=CARGO_PKG_VERSION");
 
+	build_cli();
+	build_flags();
+}
+
+/// # Build CLI Args.
+fn build_cli() {
 	let mut builder = KeyWordsBuilder::default();
 	builder.push_keys([
 		"--disable",
@@ -24,6 +33,19 @@ pub fn main() {
 	]);
 	builder.push_keys_with_values(["-c", "--config"]);
 	builder.save(out_path("argyle.rs"));
+}
+
+/// # Build Flags.
+fn build_flags() {
+	FlagsBuilder::new("Flags")
+		.private()
+		.with_flag("Disable", Some("# Disable Shitlist."))
+		.with_flag("Quiet", None)
+		.with_flag("Show", Some("# Show Only."))
+		.with_flag("Stdout", Some("# Print to STDOUT."))
+		.with_complex_flag("Systemd", ["Yes"], Some("# Systemd Use.\n\nImplies `--yes`."))
+		.with_flag("Yes", Some("# Assume Yes (Don't Prompt)."))
+		.save(out_path("flags.rs"));
 }
 
 /// # Output Path.
