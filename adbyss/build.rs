73ee658f60a352bd633b00834d95a7d8e1052e7e
Josh Stoik <josh@blobfolio.com>
2025-09-20 20:39:44 -0700

    bump: argyle 0.14

--- adbyss/build.rs
+++ adbyss/build.rs
@@ -2,42 +2,11 @@
 # Adbyss: Build
 */
 
-use argyle::{
-	FlagsBuilder,
-	KeyWordsBuilder,
-};
-use std::path::PathBuf;
-
-
-
 /// # Set Up CLI Arguments and Flags.
 fn main() {
 	println!("cargo:rerun-if-env-changed=CARGO_PKG_VERSION");
 
-	build_cli();
-	build_flags();
-}
-
-/// # Build CLI Args.
-fn build_cli() {
-	let mut builder = KeyWordsBuilder::default();
-	builder.push_keys([
-		"--disable",
-		"-h", "--help",
-		"-q", "--quiet",
-		"--show",
-		"--stdout",
-		"--systemd",
-		"-V", "--version",
-		"-y", "--yes",
-	]);
-	builder.push_keys_with_values(["-c", "--config"]);
-	builder.save(out_path("argyle.rs"));
-}
-
-/// # Build Flags.
-fn build_flags() {
-	FlagsBuilder::new("Flags")
+	argyle::FlagsBuilder::new("Flags")
 		.private()
 		.with_flag("Disable", Some("# Disable Shitlist."))
 		.with_flag("Quiet", None)
@@ -51,7 +20,7 @@ fn build_flags() {
 /// # Output Path.
 ///
 /// Append the sub-path to OUT_DIR and return it.
-fn out_path(stub: &str) -> PathBuf {
+fn out_path(stub: &str) -> std::path::PathBuf {
 	std::fs::canonicalize(std::env::var("OUT_DIR").expect("Missing OUT_DIR."))
 		.expect("Missing OUT_DIR.")
 		.join(stub)
